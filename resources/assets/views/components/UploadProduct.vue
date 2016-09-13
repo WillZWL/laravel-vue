e1251a7b1ca4699d7488fd5c7d79e54dfe7855e2
Will Zhang <will.zhang@eservicesgroup.com>
2016-09-13 15:48:55 +0800

    upload file to server

--- resources/assets/views/components/UploadProduct.vue
+++ resources/assets/views/components/UploadProduct.vue
@@ -1,24 +1,24 @@
 <style>
 .dropzone {
-    min-height: 100px;
-    border: 2px solid #e5e5e5;
-    cursor: pointer;
+  min-height: 100px;
+  border: 2px solid #e5e5e5;
+  cursor: pointer;
 }
 .fileinput-button {
-    position: relative;
-    overflow: hidden;
-    display: inline-block;
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
 }
 .fileinput-button input {
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 0;
-    opacity: 0;
-    -ms-filter: 'alpha(opacity=0)';
-    font-size: 200px !important;
-    direction: ltr;
-    cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0;
+  opacity: 0;
+  -ms-filter: 'alpha(opacity=0)';
+  font-size: 200px !important;
+  direction: ltr;
+  cursor: pointer;
 }
 .fileupload-buttonbar .btn,
 .fileupload-buttonbar .toggle {
@@ -50,267 +50,252 @@
   .fileupload-buttonbar .toggle,
   .files .toggle,
   .files .btn span {
-    display: none;
+  display: none;
   }
   .files .name {
-    width: 80px;
-    word-wrap: break-word;
+  width: 80px;
+  word-wrap: break-word;
   }
   .files audio,
   .files video {
-    max-width: 80px;
+  max-width: 80px;
   }
   .files img,
   .files canvas {
-    max-width: 100%;
+  max-width: 100%;
   }
 }
 .dropzone .dz-message {
-    text-align: center;
-    margin: 2em 0;
-    font-size: 2em;
+  text-align: center;
+  margin: 2em 0;
+  font-size: 2em;
 }
 .x_content blockquote p {
-    padding: 0;
-    margin: 0 1em;
+  padding: 0;
+  margin: 0 1em;
 }
 .x_content blockquote p i{
-    margin: 0 1em;
+  margin: 0 1em;
 }
 .x_content blockquote p a {
-    display: block;
-    line-height: 32px;
-    height: 32px;
-    border-radius: 4px;
-    cursor: pointer;
+  display: block;
+  line-height: 32px;
+  height: 32px;
+  border-radius: 4px;
+  cursor: pointer;
 }
 .x_content blockquote p a:hover fa {
-    font-size: 28px;
-    vertical-align: -6px;
+  font-size: 28px;
+  vertical-align: -6px;
 }
 .x_content blockquote p a:hover {
-    background-color: #ddd;
-    color: #fff;
-    /*padding-left:20px;*/
-    text-align: center;
-    text-decoration: none;
-    cursor: pointer;
+  background-color: #ddd;
+  color: #fff;
+  /*padding-left:20px;*/
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
 }
 
 /* Fixes for IE < 8 */
 @media screen\9 {
   .fileinput-button input {
-    filter: alpha(opacity=0);
-    font-size: 100%;
-    height: 100%;
+  filter: alpha(opacity=0);
+  font-size: 100%;
+  height: 100%;
   }
 }
 </style>
 <template>
-    <div class="page-title">
-        <div class="title_left">
-            <h3>[Product Creation]<small>  Upload CSV File For Bulk SKU Creation</small></h3>
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2>Upload a spreadsheet</h2>
+          <ul class="nav navbar-right panel_toolbox">
+            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+          </ul>
+          <div class="clearfix"></div>
         </div>
-    </div>
-    <div class="clearfix"></div>
-    <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Upload a spreadsheet</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-                    </ul>
-                    <div class="clearfix"></div>
+        <div class="x_content">
+          <blockquote>
+            <p>Send your products in comma-separated or tab-separated spreadsheet format.</p>
+            <p>Spreadsheet templates are a quick way to upload products in bulk. Edit the spreadsheet as needed. Don't change the columns.</p>
+            <p class="col-md-3 col-xs-12"><a href="#">Download Example File <strong><i class="fa fa-download"></i></strong></a></p>
+          <div class="clearfix"></div>
+          </blockquote>
+          <!-- The file upload form used as target for the file upload widget -->
+          <form id="fileupload" action="http://vanguard/api/product-upload" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="access_token" value="">
+            <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
+            <div class="row fileupload-buttonbar">
+              <div class="col-lg-7">
+                <!-- The fileinput-button span is used to style the file input field as button -->
+                <span class="btn btn-success fileinput-button">
+                  <i class="glyphicon glyphicon-plus"></i>
+                  <span>Add files...</span>
+                  <input type="file" name="files[]" multiple>
+                </span>
+                <button type="submit" class="btn btn-primary start">
+                  <i class="glyphicon glyphicon-upload"></i>
+                  <span>Start upload</span>
+                </button>
+                <button type="reset" class="btn btn-warning cancel">
+                  <i class="glyphicon glyphicon-ban-circle"></i>
+                  <span>Cancel upload</span>
+                </button>
+                <!-- The global file processing state -->
+                <span class="fileupload-process"></span>
+              </div>
+              <!-- The global progress state -->
+              <div class="col-lg-5 fileupload-progress fade">
+                <!-- The global progress bar -->
+                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar progress-bar-success" style="width:0%;"></div>
                 </div>
-                <div class="x_content">
-                    <blockquote>
-                        <p>Send your products in comma-separated or tab-separated spreadsheet format.</p>
-                        <p>Spreadsheet templates are a quick way to upload products in bulk. Edit the spreadsheet as needed. Don't change the columns.</p>
-                        <p class="col-md-3 col-xs-12"><a href="#">Download Example File <strong><i class="fa fa-download"></i></strong></a></p>
-                    <div class="clearfix"></div>
-                    </blockquote>
-                    <!-- The file upload form used as target for the file upload widget -->
-                    <form id="fileupload" action="http://vanguard/api/product-upload" method="POST" enctype="multipart/form-data">
-                        <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-                        <div class="row fileupload-buttonbar">
-                            <div class="col-lg-7">
-                                <!-- The fileinput-button span is used to style the file input field as button -->
-                                <span class="btn btn-success fileinput-button">
-                                    <i class="glyphicon glyphicon-plus"></i>
-                                    <span>Add files...</span>
-                                    <input type="file" name="files[]" multiple>
-                                </span>
-                                <button type="submit" class="btn btn-primary start">
-                                    <i class="glyphicon glyphicon-upload"></i>
-                                    <span>Start upload</span>
-                                </button>
-                                <button type="reset" class="btn btn-warning cancel">
-                                    <i class="glyphicon glyphicon-ban-circle"></i>
-                                    <span>Cancel upload</span>
-                                </button>
-                                <!-- <button type="button" class="btn btn-danger delete">
-                                    <i class="glyphicon glyphicon-trash"></i>
-                                    <span>Delete</span>
-                                </button>
-                                <input type="checkbox" class="iCheck-helper"> -->
-                                <!-- The global file processing state -->
-                                <span class="fileupload-process"></span>
-                            </div>
-                            <!-- The global progress state -->
-                            <div class="col-lg-5 fileupload-progress fade">
-                                <!-- The global progress bar -->
-                                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                                    <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-                                </div>
-                                <!-- The extended global progress state -->
-                                <div class="progress-extended">&nbsp;</div>
-                            </div>
-                        </div>
-                        <!-- The table listing the files available for upload/download -->
-                        <table role="presentation" class="table table-striped">
-                            <tbody class="files"></tbody>
-                        </table>
-                    </form>
-                    <div class="dropzone">
-                        <div class="dz-default dz-message"><span>Drop files here to upload</span></div>
-                    </div>
-                    <br>
-                    <br>
+                <!-- The extended global progress state -->
+                <div class="progress-extended">&nbsp;</div>
               </div>
             </div>
+            <!-- The table listing the files available for upload/download -->
+            <table role="presentation" class="table table-striped">
+              <tbody class="files"></tbody>
+            </table>
+          </form>
+          <div class="dropzone">
+            <div class="dz-default dz-message"><span>Drop files here to upload</span></div>
+          </div>
+          <br>
+          <br>
         </div>
+      </div>
     </div>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Upload Notes</h3>
-        </div>
-        <div class="panel-body">
-            <ul>
-                <li>The maximum file size for uploads in this tool is 1024 KB&nbsp;&nbsp;<strong>  ( 1M )</strong>.</li>
-                <li>Only text files (<strong>csv , txt</strong>) are allowed in this tool.</li>
-                <li>You can <strong>drag &amp; drop</strong> files from your desktop on this webpage.</li>
-                <li>You need download the example file when you use the tool the first time.</li>
-                <li>You need any problem when you upload, please ask IT for support.</li>
-            </ul>
-        </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Upload Notes</h3>
     </div>
+    <div class="panel-body">
+      <ul>
+        <li>The maximum file size for uploads in this tool is 1024 KB&nbsp;&nbsp;<strong>  ( 1M )</strong>.</li>
+        <li>Only text files (<strong>csv , txt</strong>) are allowed in this tool.</li>
+        <li>You can <strong>drag &amp; drop</strong> files from your desktop on this webpage.</li>
+        <li>You need download the example file when you use the tool the first time.</li>
+        <li>You need any problem when you upload, please ask IT for support.</li>
+      </ul>
+    </div>
+  </div>
 <!-- The template to display files available for upload -->
 <script id="template-upload" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-upload fade">
-        <td>
-            <span class="preview"></span>
-        </td>
-        <td>
-            <p class="name">{%=file.name%}</p>
-            <strong class="error text-danger"></strong>
-        </td>
-        <td>
-            <p class="size">Processing...</p>
-            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-        </td>
-        <td>
-            {% if (!i && !o.options.autoUpload) { %}
-                <button class="btn btn-primary start" disabled>
-                    <i class="glyphicon glyphicon-upload"></i>
-                    <span>Start</span>
-                </button>
-            {% } %}
-            {% if (!i) { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
+  <tr class="template-upload fade">
+    <td>
+      <span class="preview"></span>
+    </td>
+    <td>
+      <p class="name">{%=file.name%}</p>
+      <strong class="error text-danger"></strong>
+    </td>
+    <td>
+      <p class="size">Processing...</p>
+      <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+    </td>
+    <td>
+      {% if (!i && !o.options.autoUpload) { %}
+        <button class="btn btn-primary start" disabled>
+          <i class="glyphicon glyphicon-upload"></i>
+          <span>Start</span>
+        </button>
+      {% } %}
+      {% if (!i) { %}
+        <button class="btn btn-warning cancel">
+          <i class="glyphicon glyphicon-ban-circle"></i>
+          <span>Cancel</span>
+        </button>
+      {% } %}
+    </td>
+  </tr>
 {% } %}
 </script>
 <!-- The template to display files available for download -->
 <script id="template-download" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-download fade">
-        <td>
-            <span class="preview">
-                {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-                {% } %}
-            </span>
-        </td>
-        <td>
-            <p class="name">
-                {% if (file.url) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-                {% } else { %}
-                    <span>{%=file.name%}</span>
-                {% } %}
-            </p>
-            {% if (file.error) { %}
-                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-        <td>
-            <span class="size">{%=o.formatFileSize(file.size)%}</span>
-        </td>
-        <td>
-            {% if (file.deleteUrl) { %}
-                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                    <i class="glyphicon glyphicon-trash"></i>
-                    <span>Delete</span>
-                </button>
-            {% } else { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
+  <tr class="template-download fade">
+    <td>
+      <span class="preview">
+        {% if (file.thumbnailUrl) { %}
+          <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+        {% } %}
+      </span>
+    </td>
+    <td>
+      <p class="name">
+        {% if (file.url) { %}
+          <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
+        {% } else { %}
+          <span>{%=file.name%}</span>
+        {% } %}
+      </p>
+      {% if (file.error) { %}
+        <div><span class="label label-danger">Error</span> {%=file.error%}</div>
+      {% } %}
+    </td>
+    <td>
+      <span class="size">{%=o.formatFileSize(file.size)%}</span>
+    </td>
+    <td>
+      {% if (file.deleteUrl) { %}
+        <div class="btn btn-success">
+          <span>Success</span>
+        </div>
+      {% } else { %}
+        <button class="btn btn-warning cancel">
+          <i class="glyphicon glyphicon-ban-circle"></i>
+          <span>Cancel</span>
+        </button>
+      {% } %}
+    </td>
+  </tr>
 {% } %}
 </script>
 </template>
 
 <script>
 export default {
-    ready() {
-        this.initUpload()
-    },
-    data() {
-        return {
-            files: {}
-        }
-    },
-    methods: {
-        initUpload() {
-            $('#fileupload').fileupload({
-                // Uncomment the following to send cross-domain cookies:
-                xhrFields: {withCredentials: true},
-                url: 'http://vanguard/api/product-upload?access_token=JlzQcW3PEZP2fmd5G4ZILMfXMXmmDSCd5sxzlQ40'
-            });
-
-            // Load existing files:
-            $('#fileupload').addClass('fileupload-processing');
-            $.ajax({
-                // xhrFields: {withCredentials: true},
-                url: $('#fileupload').fileupload('option', 'url'),
-                data:{access_token:'JlzQcW3PEZP2fmd5G4ZILMfXMXmmDSCd5sxzlQ40'},
-                dataType: 'json',
-                crossDomain:true,
-                type:"POST",
-                context: $('#fileupload')[0]
-            }).always(function () {
-                $(this).removeClass('fileupload-processing');
-            }).done(function (result) {
-                console.log(result);
-                $(this).fileupload('option', 'done')
-                    .call(this, $.Event('done'), {result: result});
-            });
-            $('input.iCheck-helper').iCheck({
-                checkboxClass: 'icheckbox_flat-green',
-                radioClass: 'iradio_flat-green'
-            });
-        }
+  ready() {
+    this.initUpload()
+  },
+  data() {
+    return {
+      files: {}
     }
+  },
+  methods: {
+    initUpload() {
+      $("input[name='access_token']").val('JlzQcW3PEZP2fmd5G4ZILMfXMXmmDSCd5sxzlQ40');
+
+      $('#fileupload').fileupload({
+        url: 'http://vanguard/api/product-upload?access_token=JlzQcW3PEZP2fmd5G4ZILMfXMXmmDSCd5sxzlQ40'
+      });
+
+      // Load existing files:
+      $('#fileupload').addClass('fileupload-processing');
+
+      $.ajax({
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        crossDomain:true,
+        context: $('#fileupload')[0]
+      }).always(function () {
+        $(this).removeClass('fileupload-processing');
+      }).done(function (result) {
+        $(this).fileupload('option', 'done').call(this, $.Event('done'), {result: result});
+      });
+      $('input.iCheck-helper').iCheck({
+        checkboxClass: 'icheckbox_flat-green',
+        radioClass: 'iradio_flat-green'
+      });
+    }
+  }
 }
 </script>
