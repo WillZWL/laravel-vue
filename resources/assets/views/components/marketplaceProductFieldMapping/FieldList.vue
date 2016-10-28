220cae38d5d69c1a429b8ddc16842ec41a88fabc
Will <will.zhang@eservicesgroup.com>
2016-10-28 14:38:16 +0800

    Marketplace product field mapping

---
+++ resources/assets/views/components/marketplaceProductFieldMapping/FieldList.vue
@@ -0,0 +1,30 @@
+<template>
+<table id="{{ id }}" class="table table-striped table-bordered bulk_action jambo_table" width="100%">
+  <thead>
+    <tr>
+      <th width="15%">Marketplace Field</th>
+      <th width="15%">ESG System Field</th>
+      <th width="10%">Relate ESG Table</th>
+      <th width="30%">Description</th>
+      <th width="30%">Fixed Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="field in lists">
+      <td>{{ field.mp_field }}</td>
+      <td>{{ field.system_field }}</td>
+      <td>{{ field.table }}</td>
+      <td>{{ field.desc }}</td>
+      <td>{{ field.fixed_value }}</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+<script>
+  export default {
+    props: [
+      'id',
+      'lists'
+    ]
+  }
+</script>
---
+++ resources/assets/views/components/marketplaceProductFieldMapping/NeweggField.js
@@ -0,0 +1,194 @@
+'use strict'
+
+export var neweggList =
+[
+    {
+        'mp_field': 'SubCategoryID',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'Action',
+        'system_field': '',
+        'table': '',
+        'desc': "can use 'Update Item', 'Update/Append Image' or 'Replace Image'",
+        'fixed_value': 'Create Item / Update Item'
+    },
+    {
+        'mp_field': 'SellerPartNumber',
+        'system_field': 'sku',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'Manufacturer',
+        'system_field': 'name',
+        'table': 'brand',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ManufacturerPartNumber',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'UPC',
+        'system_field': 'upc',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'WebsiteShortTitle',
+        'system_field': 'prod_name',
+        'table': 'product_content',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'BulletDescription',
+        'system_field': 'feature',
+        'table': 'product_features',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ProductDescription',
+        'system_field': 'detail_desc',
+        'table': 'product_content',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ItemLength',
+        'system_field': 'length',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ItemWidth',
+        'system_field': 'width',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ItemHeight',
+        'system_field': 'height',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ItemWeight',
+        'system_field': 'weight',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ItemCondition',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': 'New'
+    },
+    {
+        'mp_field': 'PacksOrSets',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ShippingRestriction',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': 'No'
+    },
+    {
+        'mp_field': 'Currency',
+        'system_field': 'platform_currency_id',
+        'table': 'platform_biz_var',
+        'desc': 'Selling Currency - List in which Country Newegg',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'MSRP',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': '0'
+    },
+    {
+        'mp_field': 'MAP',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': '0'
+    },
+    {
+        'mp_field': 'SellerPartNumber',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'SellingPrice',
+        'system_field': 'price',
+        'table': 'price',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'Shipping',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': 'Default'
+    },
+    {
+        'mp_field': 'Invenory',
+        'system_field': 'surplus_qty',
+        'table': 'product',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'ActivationMark',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': 'TRUE'
+    },
+    {
+        'mp_field': 'ImageUrl',
+        'system_field': 'alt_text',
+        'table': 'product_image',
+        'desc': '',
+        'fixed_value': ''
+    },
+    {
+        'mp_field': 'IsPrimary',
+        'system_field': '',
+        'table': '',
+        'desc': '',
+        'fixed_value': '1'
+    },
+    {
+        'mp_field': 'SubCategoryProperty',
+        'system_field': '',
+        'table': '',
+        'desc': 'Based on SubCategory chosen, Each Subcatgory has its required fields',
+        'fixed_value': ''
+    },
+]
